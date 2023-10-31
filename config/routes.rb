639ff0ce7ab5d199b341a4ac7881c61efe0848c8
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-31 14:22:07 -0300

    Modify the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'root/index'
-  get 'greetings/api'
+    get 'api/greetings', to: 'greetings#api'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'root#index'
 end
