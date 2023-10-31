007d43ac7146a962ea2c3889e8b30fdb94399536
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-31 14:28:12 -0300

    Add seed to fetch greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(message: 'Hello fellow coder! Together, we create magic.') # English
+Greeting.create(message: 'Salut, collègue codeur! Ensemble, nous créons de la magie.') # French
+Greeting.create(message: '¡Hola, compañero programador! Juntos, creamos magia.') # Spanish
+Greeting.create(message: 'Ciao, caro programmatore! Insieme, creiamo magia.') # Italian
+Greeting.create(message: 'Hallo, mede-ontwikkelaar! Samen creëren we magie.') # Dutch
+Greeting.create(message: 'Olá, colega programador! Juntos, criamos magia.') # Portuguese
+Greeting.create(message: 'こんにちは、仲間のコーダー。一緒に、私たちは魔法を生み出します。') # Japanese
+Greeting.create(message: 'مرحبًا، زميل المبرمج! معًا، نخلق السحر.') # Arabic
+Greeting.create(message: 'Здраво, колега програмеру! Заједно стварамо магију.') # Serbian
+Greeting.create(message: 'Hej, medkodare! Tillsammans skapar vi magi.') # Swedish
+Greeting.create(message: "Aloha, hoa kākou me ka lāhui kākoʻo. Pūpūkāhi i holomua!") # Hawaiian (Translated: "Aloha, fellow coder. Together we progress!")
+Greeting.create(message: 'Hei, kanssakoodari! Yhdessä luomme taikaa.') # Finnish
+Greeting.create(message: 'Hei, medkoder! Sammen skaper vi magi.') # Norwegian
+Greeting.create(message: 'Hallo, lieber Programmierer! Zusammen erschaffen wir Magie.') # German
+Greeting.create(message: 'Здраво, колега програмеру! Заједно стварамо чудо.') # Serbian (Cyrillic)
+Greeting.create(message: 'Dzień dobry, drodzy programiści. Razem tworzymy magię!') # Polish
+Greeting.create(message: 'Γεια σου, συνάδελφε προγραμματιστή! Μαζί δημιουργούμε μαγεία.') # Greek
+Greeting.create(message: '안녕하세요, 동료 개발자 여러분. 함께 마법을 만들어요!') # Korean
+Greeting.create(message: 'שלום, חבר המתכנת! ביחד אנחנו יוצרים קסם.') # Hebrew
+Greeting.create(message: 'Merhaba, meslektaş geliştirici! Birlikte sihir yaratıyoruz.') # Turkish
